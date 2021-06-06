3902f4fd25a51e44a02a4f66df65a9c48b5f6244
Pascal Helmer <pascal@pascalhelmer.de>
2021-06-06 12:37:42 +0200

    feat: add example apps

---
+++ apps/vue-old/jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  displayName: 'vue-old',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+  coverageDirectory: '../../coverage/apps/vue-old',
+  snapshotSerializers: ['jest-serializer-vue'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      babelConfig: '<rootDir>/babel.config.js',
+    },
+    'vue-jest': {
+      tsConfig: `${__dirname}/tsconfig.spec.json`,
+      babelConfig: `${__dirname}/babel.config.js`,
+    },
+  },
+};
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  projects: ['<rootDir>/apps/web-components'],
+  projects: [
+    '<rootDir>/apps/web-components',
+    '<rootDir>/apps/angular',
+    '<rootDir>/apps/react',
+    '<rootDir>/apps/vue',
+    '<rootDir>/apps/vue-old',
+  ],
 };
